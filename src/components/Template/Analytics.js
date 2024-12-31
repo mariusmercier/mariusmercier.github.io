e671f693ce67eafd23899b72388745766c0368c4
mariusmercier <mariusmercier1@gmail.com>
2024-12-31 14:35:57 +0100

    fix ga4

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -2,19 +2,24 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 
-const { NODE_ENV, REACT_APP_GA_TRACKING_ID } = process.env;
+const { NODE_ENV } = process.env;
 
-if (NODE_ENV === 'production') {
-  ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
+const REACT_APP_GA_TRACKING_ID = process.env.REACT_APP_GA_TRACKING_ID || 'G-F60T133RWZ';
+
+if (NODE_ENV === 'production' && REACT_APP_GA_TRACKING_ID) {
+  ReactGA.initialize(REACT_APP_GA_TRACKING_ID, {
+    gaOptions: {
+      debug_mode: false
+    }
+  });
 }
 
 const Analytics = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    if (NODE_ENV === 'production') {
-      // GA4 uses send instead of set and has a different event structure
-      ReactGA.send({
+    if (NODE_ENV === 'production' && REACT_APP_GA_TRACKING_ID) {
+      ReactGA.send({ 
         hitType: 'pageview',
         page: pathname,
       });
