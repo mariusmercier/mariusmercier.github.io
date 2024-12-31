f936b688f62fd75376c90680d9c1735109def0b0
mariusmercier <mariusmercier1@gmail.com>
2024-12-31 14:48:11 +0100

    test

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -33,7 +33,7 @@ const Resume = () => (
       <h3>My CV</h3>
       <iframe
         title="Resume"
-        src="/resume.pdf"
+        src="/personal-site/resume.pdf"
         width="100%"
         height="900px"
         style={{ border: 0 }}
