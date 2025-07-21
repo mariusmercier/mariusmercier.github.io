e3c7e637d3bb5e3a7b10f22d6e5460fcb130e367
mariusmercier <mariusmercier1@gmail.com>
2025-07-21 12:11:42 +0200

    up

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/href-no-hash': 'off',
     'jsx-a11y/no-static-element-interactions': 0,
-    'linebreak-style': ['error', os.EOL === '\r\n' ? 'windows' : 'unix'],
+    'linebreak-style': 0, // Disabled to avoid cross-platform issues
     'no-console': [
       'error',
       {
